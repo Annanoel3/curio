51d8ae2285e6205d0f80c682ac40b7937f107cd2
Annanoel3 <annanoel3@github.com>
2026-05-13 14:52:34 -0500

    feat: add custom Curio app icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.curio.app',
+  appId: 'com.mediocreatbest.curio',
   appName: 'Curio',
   webDir: 'dist',                   // Vite's build output folder
   server: {
